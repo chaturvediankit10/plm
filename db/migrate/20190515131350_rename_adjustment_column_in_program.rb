e7571c00d29e556cc410f2243bf2a3a98c72aba0
Shiv <shivpatel779@gmail.com>
2019-05-17 17:42:36 +0530

    remove migration

--- db/migrate/20190515131350_rename_adjustment_column_in_program.rb
+++
@@ -1,8 +0,0 @@
-class RenameAdjustmentColumnInProgram < ActiveRecord::Migration[5.1]
-  def change
-  	rename_column :programs, :adjustments, :adjustment_ids
-  	rename_column :programs, :du, :fannie_mae_du
-  	rename_column :programs, :lp, :freddie_mac_lp
-  	add_column :programs, :arm_caps, :string
-  end
-end
