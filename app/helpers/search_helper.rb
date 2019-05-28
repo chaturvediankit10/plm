e46488965e7c26a3bf8caf53357bad356de7c535
Shiv <shivpatel779@gmail.com>
2019-05-28 20:13:19 +0530

    added search for tabs

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,9 @@
 module SearchHelper
+	def get_results(result, term)
+		return result.map{|t| t if t[:term] == term}.compact
+	end
+
+	def get_results_arm(result,arm)
+		return result.map{|t| t if t[:arm_basic] == arm}.compact
+	end
 end
