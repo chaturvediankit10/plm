e86341ce9e54fb316abac2299bffbb4365c51e14
Shiv <pshiv8759@gmail.com>
2019-06-11 12:17:13 +0530

    Working on Tab functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < SearchApi::DashboardController
   
   def home
-    list_of_banks_and_programs_with_search_results    
+    list_of_banks_and_programs_with_search_results
+    debugger
+    respond_to do |format|
+	    format.html
+	    format.js # actually means: if the client ask for js -> return file.js
+	  end
   end
 
   def fetch_programs
