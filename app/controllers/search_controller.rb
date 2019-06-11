d4c8c93de7404e50dbca9252f01226edc0943af5
Shiv <pshiv8759@gmail.com>
2019-06-11 19:18:11 +0530

    fix 30yr, 15yr and 5/1 arm tab issue

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,6 @@ class SearchController < SearchApi::DashboardController
   
   def home
     list_of_banks_and_programs_with_search_results
-    debugger
     respond_to do |format|
 	    format.html
 	    format.js # actually means: if the client ask for js -> return file.js
