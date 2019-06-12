8f93d5444840aaaf8ba240cf9da061bda9b2f8d8
Shiv <shivpatel779@gmail.com>
2019-06-12 18:30:31 +0530

    fix Tab issue and added loader

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,12 @@ class SearchController < SearchApi::DashboardController
   
   def home
     list_of_banks_and_programs_with_search_results
+    if params[:arm_basic].present?
+      @arm_term = 51
+    else
+      @arm_term = params[:term].present? ? params[:term] : @term
+    end
+    
     respond_to do |format|
 	    format.html
 	    format.js # actually means: if the client ask for js -> return file.js
