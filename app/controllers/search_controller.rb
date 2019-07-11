553e5455edd5c26f0de7776f354511229233d517
Virendra <virendrayuvasoft221@gmail.com>
2019-07-11 20:07:12 +0530

    fix zip code loading issue on web app

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,25 @@
 class SearchController < SearchApi::DashboardController
   include Onload
   def home
-    set_default
     api_search
+    initilize_state_and_zip_code
     if params[:loan_type] == "ARM" && params[:arm_basic].present?
       @arm_term = 51
     else
       @arm_term = params[:term].present? ? params[:term] : @term
     end
-    
     respond_to do |format|
 	    format.html
 	    format.js # actually means: if the client ask for js -> return file.js
 	  end
+  end
+
+  def initilize_state_and_zip_code
+    @zip_code = "88901"
+    results = Geocoder.search(request.ip)
+    @zip_code = results.first.postal_code if results.first.present?
+    city = City.find_by_zip(@zip_code.to_i) if @zip_code.present?
+    @state = city.present? ? city.state_code : "All"
   end
 
   def fetch_programs
@@ -20,16 +27,10 @@ class SearchController < SearchApi::DashboardController
   end
 
   def set_state_by_zip_code
-    zip_code = ''
-    if params[:geo_coder].present?
-      results = Geocoder.search(request.ip)
-      zip_code = results.first.postal_code if results.first.present?
-    else
-      zip_code = params[:zip].to_i if params[:zip].present?
-    end
-    city = City.find_by_zip(zip_code.to_i) if zip_code.present?
+    @zip_code = params[:zip].to_i if params[:zip].present?
+    city = City.find_by_zip(@zip_code.to_i) if @zip_code.present?
     respond_to do |format|
-      format.json {render :json => {state: city.present? ? city.state_code : "All", zip_code: zip_code }}
+      format.json {render :json => {state: city.present? ? city.state_code : "All", zip_code: @zip_code }}
     end
   end
 end
